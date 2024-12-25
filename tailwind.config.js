475c747fa7afaa5cb262e5ca63e7a581077c89ce
bastienggg <bastien.guitard@etu.unilim.fr>
2024-12-25 16:53:23 +0100

    ajout de vite et reconfiguartion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ajoutez vos chemins ici
   theme: {
-    colors: {
-      'white': 'hsl(var(--clr-white) / <alpha-value>)',
-      'bg-red': 'hsl(var(--clr-red-bg) / <alpha-value>)',
-      'btn-red': 'hsl(var(--clr-red-btn) / <alpha-value>)',
-      'bg-blue': 'hsl(var(--clr-blue-bg) / <alpha-value>)',
-      'btn-blue': 'hsl(var(--clr-blue-btn) / <alpha-value>)',
-      'bg-green': 'hsl(var(--clr-green-bg) / <alpha-value>)',
-      'btn-green': 'hsl(var(--clr-green-btn) / <alpha-value>)',
-    }
+    extend: {},
   },
   plugins: [],
 }
-
